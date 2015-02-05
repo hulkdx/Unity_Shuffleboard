74c8d709b5844fba5a93db37bcc75898e339b764
hulkdx <hulktroll@gmail.com>
2015-02-05 17:44:06 +0200

    shuffleboard

---
+++ Assets/Scripts/Scores/P2ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class P2ScoreManager : MonoBehaviour {
+	public static int score;
+	Text text;
+	
+	// Use this for initialization
+	void Awake () {
+		text = GetComponent <Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = score.ToString();
+	}
+}
---
+++ Assets/Scripts/Scores/Rounds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Rounds : MonoBehaviour {
+	public static int round;
+	Text text;
+
+	void Awake () {
+		text = GetComponent <Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = round.ToString();
+	}
+}
